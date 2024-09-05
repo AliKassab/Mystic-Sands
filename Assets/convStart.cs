c308d9f981e5d3e0e3c691f60bf900b00803a2b8
Ali Ghaleb <alighaleb2001@gmail.com>
2024-09-05 23:19:22 +0300

    Vfx_skull
    Vfx_skull

--- Assets/convStart.cs
+++ Assets/convStart.cs
@@ -3,26 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
 using System;
+using UnityEngine.UI;
 
 public class convStart : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
-
+    public float delay = 1f;
     public GameObject smoke;
+    public GameObject skullModel;
+    public Animator anim_Skull;
+    public string animationClipName = "skull";
+    public TMPro.TextMeshProUGUI interactText;
+    private void Start()
+    {
+        skullModel.SetActive(false);
+        interactText.gameObject.SetActive(false);  
+    }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (Input.GetKeyDown(KeyCode.E)){
-                Console.WriteLine("EEEEEEEEE");
+            interactText.gameObject.SetActive(true); 
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
                 print("E pressed");
-                Instantiate(smoke, transform.position, Quaternion.identity);
+
+                skullModel.SetActive(true);
+                anim_Skull.Play(animationClipName);
+
+                StartCoroutine(DelayedSmokeInstantiation());
+
                 ConversationManager.Instance.StartConversation(myConversation);
+
+                interactText.gameObject.SetActive(false);  
             }
-           
         }
     }
 
-    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interactText.gameObject.SetActive(false); 
+        }
+    }
+
+    private IEnumerator DelayedSmokeInstantiation()
+    {
+        yield return new WaitForSeconds(delay);
+        Instantiate(smoke, transform.position, Quaternion.identity);
+    }
 }
